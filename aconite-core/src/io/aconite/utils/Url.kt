0597b93cb09435efd4aeffd39855751cf81afe21
ashlanderr <aleksandr.schilow2012@yandex.ru>
2017-05-20 11:05:52 +0300

    rename ComparableUrl to UrlTemplate, add parsing

--- aconite-core/src/io/aconite/utils/Url.kt
+++ aconite-core/src/io/aconite/utils/Url.kt
@@ -1,5 +1,131 @@
 package io.aconite.utils
 
-class UrlFormatException: Exception()
+private val URL_PARAM_REGEX = Regex("^\\{(?<param>[^/{}]+)}|^(?<text>[^{}]+)")
+private val URL_VALIDATION_REGEX = Regex("^(?:\\{[^/{}]+}|[^{}]+)+$")
+
+class UrlFormatException(message: String): Exception(message)
 
 fun formatUrl(url: String) = '/' + url.trim('/')
+
+class UrlTemplate(url: String): Comparable<UrlTemplate> {
+    private val parts: List<UrlPart>
+    private val params: List<String>
+    private val regex: Regex
+
+    init {
+        val formattedUrl = formatUrl(url)
+        validateUrl(formattedUrl)
+        val (parts, params) = buildParts(formattedUrl)
+        this.parts = parts
+        this.params = params
+        this.regex = Regex("^" + parts.map { it.toRegex() }.joinToString(""))
+    }
+
+    fun parse(url: String): Pair<String, Map<String, String>>? {
+        val match = regex.find(url) ?: return null
+        return parseInner(match, url)
+    }
+
+    fun parseEntire(url: String): Pair<String, Map<String, String>>? {
+        val match = regex.matchEntire(url) ?: return null
+        return parseInner(match, url)
+    }
+
+    private fun parseInner(match: MatchResult, url: String): Pair<String, Map<String, String>> {
+        val values = params
+                .mapIndexed { idx, name -> Pair(name, match.groups[idx + 1]?.value) }
+                .mapNotNull { it.allOrNull() }
+                .toMap()
+        val parsedUrl = url.drop(match.range.last + 1)
+        return Pair(parsedUrl, values)
+    }
+
+    private fun validateUrl(url: String) {
+        if (!URL_VALIDATION_REGEX.matches(url))
+            throw UrlFormatException("String '$url' is not url")
+    }
+
+    private fun buildParts(url: String): Pair<List<UrlPart>, List<String>> {
+        var s = url
+        val parts = mutableListOf<UrlPart>()
+        val params = mutableListOf<String>()
+
+        while (s != "") {
+            val part = URL_PARAM_REGEX.find(s)!! // checked to be not null by validation
+            val gParam = part.groups["param"]
+            val gText = part.groups["text"]
+
+            assert(gParam != null || gText != null)
+            if (gParam != null) {
+                parts.add(ParameterUrlPart())
+                params.add(gParam.value)
+            } else if (gText != null) {
+                parts.add(TextUrlPart(gText.value))
+            }
+
+            s = s.drop(part.range.last + 1)
+        }
+
+        return Pair(parts, params)
+    }
+
+    private fun getPart(index: Int) = if (index < parts.size) this.parts[index] else EmptyUrlPart
+
+    override fun compareTo(other: UrlTemplate): Int {
+        val size = maxOf(this.parts.size, other.parts.size)
+        var i = 0
+        var r = 0
+
+        while (i < size && r == 0) {
+            val a = this.getPart(i)
+            val b = other.getPart(i)
+            r = a.compareTo(b)
+            ++i
+        }
+
+        return r
+    }
+}
+
+private interface UrlPart: Comparable<UrlPart> {
+    fun toRegex(): String
+}
+
+private class TextUrlPart(val text: String): UrlPart {
+    override fun compareTo(other: UrlPart): Int {
+        return when (other) {
+            is TextUrlPart -> this.text.compareTo(other.text)
+            is ParameterUrlPart -> 1
+            is EmptyUrlPart -> 1
+            else -> throw NotImplementedError()
+        }
+    }
+
+    override fun toRegex() = Regex.escape(text)
+}
+
+private class ParameterUrlPart: UrlPart {
+    override fun compareTo(other: UrlPart): Int {
+        return when (other) {
+            is TextUrlPart -> -1
+            is ParameterUrlPart -> 0
+            is EmptyUrlPart -> 1
+            else -> throw NotImplementedError()
+        }
+    }
+
+    override fun toRegex() = "([^/]+)"
+}
+
+private object EmptyUrlPart: UrlPart {
+    override fun compareTo(other: UrlPart): Int {
+        return when (other) {
+            is TextUrlPart -> -1
+            is ParameterUrlPart -> -1
+            is EmptyUrlPart -> 0
+            else -> throw NotImplementedError()
+        }
+    }
+
+    override fun toRegex() = ""
+}
---
+++ aconite-core/tests/io/aconite/utils/UrlTemplateTest.kt
@@ -0,0 +1,105 @@
+package io.aconite.utils
+
+import org.junit.Assert
+import org.junit.Test
+
+class UrlTemplateTest {
+
+    @Test
+    fun testTextGreaterThanParam() {
+        val a = UrlTemplate("123/foo/text/baz")
+        val b = UrlTemplate("123/foo/{bar}/baz")
+        Assert.assertTrue(a > b)
+    }
+
+    @Test
+    fun testParamLessThanText() {
+        val a = UrlTemplate("123/foo/{bar}/baz")
+        val b = UrlTemplate("123/foo/text/baz")
+        Assert.assertTrue(a < b)
+    }
+
+    @Test
+    fun testMoreSpecificUrlIsGreater() {
+        val a = UrlTemplate("123/foo/text/baz")
+        val b = UrlTemplate("123/foo/text")
+        Assert.assertTrue(a > b)
+    }
+
+    @Test
+    fun testTwoParamsGreaterThanOne() {
+        val a = UrlTemplate("123/{foo}/{bar}")
+        val b = UrlTemplate("123/{foo}")
+        Assert.assertTrue(a > b)
+    }
+
+    @Test
+    fun testEmptyLessThanText() {
+        val a = UrlTemplate("")
+        val b = UrlTemplate("123/bar")
+        Assert.assertTrue(a < b)
+    }
+
+    @Test
+    fun testEmptyLessThanParam() {
+        val a = UrlTemplate("")
+        val b = UrlTemplate("{baz}")
+        Assert.assertTrue(a < b)
+    }
+
+    @Test
+    fun testSort() {
+        val urls = listOf(
+                UrlTemplate("/sequences/{name}/items/last"),
+                UrlTemplate("/sequences/{name}/items/first"),
+                UrlTemplate("/sequences/{name}/items/{key}"),
+                UrlTemplate("/sequences/{name}/items"),
+                UrlTemplate("/sequences/{name}"),
+                UrlTemplate("/sequences")
+        )
+        Assert.assertEquals(urls.reversed(), urls.sorted())
+    }
+
+    @Test(expected = UrlFormatException::class)
+    fun testNotValidUrl() {
+        UrlTemplate("/foo/{bar{baz}")
+    }
+
+    @Test
+    fun testPathPartMatch() {
+        val template = UrlTemplate("/foo/bar")
+        val (rest, _) = template.parse("/foo/bar/baz")!!
+        Assert.assertEquals("/baz", rest)
+    }
+
+    @Test
+    fun testPathFullMatch() {
+        val template = UrlTemplate("/foo/bar/baz")
+        val (rest, _) = template.parseEntire("/foo/bar/baz")!!
+        Assert.assertEquals("", rest)
+    }
+
+    @Test
+    fun testPathFullNotMatch() {
+        val template = UrlTemplate("/foo/bar")
+        val result = template.parseEntire("/foo/bar/baz")
+        Assert.assertNull(result)
+    }
+
+    @Test
+    fun testPathManyArgs() {
+        val template = UrlTemplate("/foo/{arg1}/{arg2}/{arg3}")
+        val (_, values) = template.parseEntire("/foo/bar/baz/123")!!
+        Assert.assertEquals(3, values.size)
+        Assert.assertEquals("bar", values["arg1"])
+        Assert.assertEquals("baz", values["arg2"])
+        Assert.assertEquals("123", values["arg3"])
+    }
+
+    @Test
+    fun testPathEmptyMatch() {
+        val template = UrlTemplate("")
+        val result = template.parseEntire("/")!!
+        Assert.assertNotNull(result)
+    }
+}
