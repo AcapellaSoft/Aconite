e56d4347fb2ebcd7d2cace5b0d8d247f1791f966
ashlanderr <aleksandr.schilow2012@yandex.ru>
2017-07-03 14:12:51 +0300

    Tests for StringSerializerFactory

---
+++ aconite-server/tests/io/aconite/server/serializers/StringSerializerTest.kt
@@ -0,0 +1,33 @@
+package io.aconite.server.serializers
+
+import io.aconite.server.EmptyAnnotations
+import org.junit.Assert
+import org.junit.Test
+import java.util.*
+import kotlin.reflect.full.createType
+
+class StringSerializerTest {
+    @Test fun testString() {
+        val serializer = StringSerializerFactory().create(EmptyAnnotations, String::class.createType())!!
+
+        Assert.assertEquals("foobar", serializer.serialize("foobar"))
+        Assert.assertEquals("foobar", serializer.deserialize("foobar"))
+    }
+
+    @Test fun testInt() {
+        val serializer = StringSerializerFactory().create(EmptyAnnotations, Int::class.createType())!!
+
+        Assert.assertEquals("123", serializer.serialize(123))
+        Assert.assertEquals(123, serializer.deserialize("123"))
+    }
+
+    @Test fun testUUID() {
+        val serializer = StringSerializerFactory().create(EmptyAnnotations, UUID::class.createType())!!
+
+        val expected = UUID.randomUUID()
+        val string = serializer.serialize(expected)!!
+        val actual = serializer.deserialize(string)
+        Assert.assertEquals(expected, actual)
+        Assert.assertEquals(expected.toString(), string)
+    }
+}
