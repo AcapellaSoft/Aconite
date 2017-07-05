f9e4bde6e6f2eb9044ee4f8d6a1cc0b7bd56a5a0
ashlanderr <aleksandr.schilow2012@yandex.ru>
2017-07-05 15:17:49 +0300

    Documentation for string and body serializers

--- aconite-core/src/io/aconite/BodySerializer.kt
+++ aconite-core/src/io/aconite/BodySerializer.kt
@@ -3,11 +3,38 @@ package io.aconite
 import kotlin.reflect.KAnnotatedElement
 import kotlin.reflect.KType
 
+/**
+ * Used to serialize and deserialize object to [BodyBuffer].
+ */
 interface BodySerializer {
+    /**
+     * Used to create [BodySerializer] for concrete type.
+     */
     interface Factory {
+        /**
+         * Creates [BodySerializer] for [type]. If this serializer does not support
+         * [type], then it must return `null`.
+         * @param[annotations] annotations of function, field or argument to be serialized
+         * @param[type] type of object to be serialized
+         * @return serializer for [type] or `null` if [type] is not supported
+         */
         fun create(annotations: KAnnotatedElement, type: KType): BodySerializer?
     }
 
+    /**
+     * Serializes [obj] to [BodyBuffer]. The [obj] must be of type, that was passed
+     * to [Factory.create] function.
+     * @param[obj] to be serialized
+     * @return serialized body
+     */
     fun serialize(obj: Any?): BodyBuffer
+
+    /**
+     * Deserializes [body] to object with type, that was passed to [Factory.create] function.
+     * @param[body] to be deserialized
+     * @return deserialized object
+     * @throws[UnsupportedMediaTypeException] if [body] content type is not supported
+     * @throws[BadRequestException] if [body] content can not be deserialized
+     */
     fun deserialize(body: BodyBuffer): Any?
 }
--- aconite-core/src/io/aconite/StringSerializer.kt
+++ aconite-core/src/io/aconite/StringSerializer.kt
@@ -1,13 +1,41 @@
 package io.aconite
 
+import io.aconite.BodySerializer.Factory
 import kotlin.reflect.KAnnotatedElement
 import kotlin.reflect.KType
 
+/**
+ * Used to serialize and deserialize object to string. String can be
+ * header, query or path parameter.
+ */
 interface StringSerializer {
+    /**
+     * Used to create [StringSerializer] for concrete type.
+     */
     interface Factory {
+        /**
+         * Creates [StringSerializer] for [type]. If this serializer does not support
+         * [type], then it must return `null`.
+         * @param[annotations] annotations of function, field or argument to be serialized
+         * @param[type] type of object to be serialized
+         * @return serializer for [type] or `null` if [type] is not supported
+         */
         fun create(annotations: KAnnotatedElement, type: KType): StringSerializer?
     }
 
+    /**
+     * Serializes [obj] to string. The [obj] must be of type, that was passed
+     * to [Factory.create] function.
+     * @param[obj] to be serialized
+     * @return serialized string
+     */
     fun serialize(obj: Any?): String?
+
+    /**
+     * Deserializes [s] to object with type, that was passed to [Factory.create] function.
+     * @param[s] to be deserialized
+     * @return deserialized object
+     * @throws[BadRequestException] if [s] content can not be deserialized
+     */
     fun deserialize(s: String): Any?
 }
