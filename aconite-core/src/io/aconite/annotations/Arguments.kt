7360f8bb11ddeb3df28d6208e923ce99523c159f
ashlanderr <aleksandr.schilow2012@yandex.ru>
2017-07-05 14:08:21 +0300

    Documentation: make reference to serializers

--- aconite-core/src/io/aconite/annotations/Arguments.kt
+++ aconite-core/src/io/aconite/annotations/Arguments.kt
@@ -1,9 +1,12 @@
 package io.aconite.annotations
 
+import io.aconite.BodySerializer
+import io.aconite.StringSerializer
+
 /**
  * The function argument marked by this annotation will contain
  * the body of the request. The value of the argument will be
- * serialized using the body serializer. If the request does
+ * serialized using the [BodySerializer]. If the request does
  * not contain this header then `null` will be passed as the
  * argument value.
  */
@@ -15,7 +18,7 @@ annotation class Body
  * The function argument marked by this annotation will contain
  * the [name] header of the request. If [name] is an empty string
  * then the argument name will be used. The argument value will
- * be serialized using the string serializer. If the request does
+ * be serialized using the [StringSerializer]. If the request does
  * not contain this header then `null` will be passed as the
  * argument value.
  */
@@ -27,7 +30,7 @@ annotation class Header(val name: String = "")
  * The function argument marked by this annotation will contain
  * the [name] path parameter of the request. If [name] is an empty
  * string then the argument name will be used. The argument
- * value will be serialized using the string serializer. The
+ * value will be serialized using the [StringSerializer]. The
  * [name] path parameter must be presented in the request.
  * Path parameters are part of the url template captured by curly
  * brackets. For example: `/users/{userId}/first-name` url contains
@@ -41,7 +44,7 @@ annotation class Path(val name: String = "")
  * The function argument marked by this annotation will contain
  * the [name] query parameter of the request. If [name] is an empty
  * string then the argument name will be used. The argument value
- * will be serialized using the string serializer. If the request
+ * will be serialized using the [StringSerializer]. If the request
  * does not contain this query then `null` will be passed as the
  * argument value.
  */
