47909ec782159bbf6f6619e44bdb57285af0e638
ashlanderr <aleksandr.schilow2012@yandex.ru>
2018-02-14 19:01:21 +0300

    Fix SimpleBodySerializer: now it can handle empty types

--- aconite-core/src/io/aconite/serializers/SimpleBodySerializer.kt
+++ aconite-core/src/io/aconite/serializers/SimpleBodySerializer.kt
@@ -6,9 +6,15 @@ import io.aconite.Buffer
 import kotlin.reflect.KAnnotatedElement
 import kotlin.reflect.KType
 
-class SimpleBodySerializer: BodySerializer {
+class SimpleBodySerializer : BodySerializer {
+    object EmptySerializer : BodySerializer {
+        override fun serialize(obj: Any?) = BodyBuffer(Buffer.wrap(""), "text/plain")
+        override fun deserialize(body: BodyBuffer): Any? = null
+    }
+
     object Factory: BodySerializer.Factory {
         override fun create(annotations: KAnnotatedElement, type: KType): BodySerializer? {
+            if (type.classifier == Unit::class || type.classifier == Void::class) return EmptySerializer
             if (type.classifier != String::class) return null
             return SimpleBodySerializer()
         }
